6412616b93e1682d60f9b645c10947c5a2fabfdc
Vipin Paneru <paneruvipinbusiness@gmail.com>
2025-10-17 00:03:29 +0530

    Change server port from 3000 to 8080

--- index.js
+++ index.js
@@ -94,5 +94,5 @@ app.get("/check-login", async (req, res) => {
 });
 
 app.get("/re-watch", reWatch);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
